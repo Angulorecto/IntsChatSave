48907abd95efd140eff849225785bb6b007b2cd8
Angulorecto <120491110+Angulorecto@users.noreply.github.com>
2023-03-22 07:44:10 -0400

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,13 @@
+var frame = getElementById("chat");
+var x = getElementById("coords");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
 
+function showPosition(position) {
+  x.innerHTML = position.coords.latitude + position.coords.longitude;
+}
