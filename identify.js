b8c550ea61677b6047342915d50e5306a5521d48
Angulorecto <120491110+Angulorecto@users.noreply.github.com>
2023-03-29 08:03:49 -0400

    Update identify.js

--- identify.js
+++ identify.js
@@ -1,15 +1,39 @@
+let ip = 0;
+fetch("https://api.ipify.org/").then(results => results.json()).then(data => sessionStorage.setItem("ip", data));
+let ip = sessionStorage.getItem("ip");
 var frame = getElementById("chat");
-var x = getElementById("coords");
-frame.style.left = "100px";
-frame.style.top = "100";
+let x = 0;
+let y = 0;
+
+let Schoolx = "";
+let Schooly = "";
+function between(num, min, max) {
+  return num >= min && num <= max;
+};
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
+  };
+};
 
 function showPosition(position) {
-  x.innerHTML = position.coords.latitude + position.coords.longitude;
-}
+  x = position.coords.latitude;
+  y = position.coords.longitude;
+};
+function setFrame() {
+  // NHMS Check
+  // x check 
+  if (between(x, 42.98967684492836, 42.99068069641312)) {
+    Schoolx = "NHMS";
+  };
+  // y check 
+  if (between(y, -85.58028245713595, -85.5784934721124)) {
+    Schooly = "NHMS";
+  };
+  if (Schoolx == "NHMS" && Schooly == "NHMS") {
+    frame.src = "https://deadsimplechat.com/1Cflie9fe";
+  };
+};
+getLocation();
+setFrame();
+alert("Script has worked");
